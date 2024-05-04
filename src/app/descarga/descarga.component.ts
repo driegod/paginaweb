7d7b715b858bccd5817926bac3699e29a2d8272d
driegod <atom.jdmq@gmail.com>
2024-05-03 22:39:40 -0600

    primer commit

---
+++ src/app/descarga/descarga.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-descarga',
+  standalone: true,
+  imports: [],
+  templateUrl: './descarga.component.html',
+  styleUrl: './descarga.component.css'
+})
+export class DescargaComponent {
+
+}
